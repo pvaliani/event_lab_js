d67a3cfa436e884b6a1653730f5e67b2727a9c9b
Pedram Valiani <p.valiani@gmail.com>
2020-12-10 16:27:57 +0000

    second commit -mvp complete

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
+  
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+  // const button = document.querySelector('#delete');
+  // button.addEventListener('click', handleButtonClick);
+
 })
+
+
+const handleFormSubmit = function (event) {
+
+  // stops the post request
+  event.preventDefault();
+
+  const title = event.target.title.value
+  const author = event.target.author.value
+  const category = event.target.category.value
+
+  // create li element i.e list
+  // 
+
+  const newListItem = document.createElement('li');
+
+  // Title
+  const newTitleItem = document.createElement('h2');
+  newTitleItem.textContent = `Title: ${title}`;
+  newListItem.appendChild(newTitleItem);
+
+  // Author
+  const newAuthorItem = document.createElement('h3');
+  newAuthorItem.textContent = `Author: ${author}`;
+  newListItem.appendChild(newAuthorItem);
+
+  // Category
+
+  const newCategoryItem = document.createElement('h4');
+  newCategoryItem.textContent = `Category: ${category}`;
+  newListItem.appendChild(newCategoryItem);
+
+  
+  const readingList = document.querySelector("#reading-list");
+
+
+  readingList.appendChild(newListItem);
+  event.target.reset();
+};
+
+
+// const handleButtonClick = function () {};
